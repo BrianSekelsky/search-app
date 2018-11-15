95b7b0b93855db6c88db531b53b6ef516866b963
Brian <BrianSekelsky@Gmail.com>
2018-11-15 09:11:10 -0600

    Updates

---
+++ precache-manifest.618b2adf97481d1a27828d41e9b317c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f39a211c8c294770581d5bfadf1836f",
+    "url": "/search-app/static/media/minimize.8f39a211.svg"
+  },
+  {
+    "revision": "bb158ab64ed266b995914babb7ecf46a",
+    "url": "/search-app/static/media/expand.bb158ab6.svg"
+  },
+  {
+    "revision": "44c14d2fa96cdba775b7",
+    "url": "/search-app/static/js/runtime~main.44c14d2f.js"
+  },
+  {
+    "revision": "5ffaab511acc5feca026",
+    "url": "/search-app/static/js/main.5ffaab51.chunk.js"
+  },
+  {
+    "revision": "3f2b8e6195e41c31919e",
+    "url": "/search-app/static/js/1.3f2b8e61.chunk.js"
+  },
+  {
+    "revision": "5ffaab511acc5feca026",
+    "url": "/search-app/static/css/main.27876256.chunk.css"
+  },
+  {
+    "revision": "a4e64dda5ab73e51c0d1d4df9e803ef8",
+    "url": "/search-app/index.html"
+  }
+];
